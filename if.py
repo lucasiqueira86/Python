bcf2a1ad7df3347e41f00d2084fc98b0c4889fd5
lucasiqueira86 <lucasprogramador86@gmail.com>
2020-07-21 16:25:03 -0300

    Update if.py

--- if.py
+++ if.py
@@ -1,8 +1,16 @@
-idade = 15
+# idade = 15
 
-if idade >= 18:
-    print('Maior de idade!')
-    else:
-         print('Menor de idade!')            
+# if idade >= 18:
+    # print('Maior de idade!')
+    # else:
+        #  print('Menor de idade!')   
+
+a = int(input('primeiro valor'))
+b = int(input('segundo valor'))    
+
+if a > b :
+    print('O primeiro valor é maiopr que o segundo')
+if b > a:
+    print('O segundo valor é maior que o primeiro')
     
     
