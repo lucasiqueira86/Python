f8af4a548354c07008b7b0ae2b3de10135d1a391
lucasiqueira86 <lucasprogramador86@gmail.com>
2020-07-21 16:15:32 -0300

    Update calculos.py

--- calculos.py
+++ calculos.py
@@ -2,3 +2,15 @@ a = 10
 b = 20
 print(a + b)
 
+nome = 'Lucas'
+idade = 33
+carro = 'ferrari'
+print(f'{nome} tem {idade} anos e comprou uam {carro}')
+
+
+numero = input('Digite um numero:')
+if numero >= '10':
+    print('numero maior que 10')
+else:
+    print('numero menor que 10')
+
