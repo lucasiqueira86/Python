5baee2057b79c4fd5da0242e8f07baf128bf9214
lucasiqueira86 <lucasprogramador86@gmail.com>
2021-12-26 15:31:03 -0300

    Create FOR2.py

---
+++ FOR2.py
@@ -0,0 +1,10 @@
+carros = [ 'porche', 'ferrari', 'fiat']
+
+for item in carros:
+    print(item.capitalize())
+
+numeros = [ 10,20,50,80,30]
+
+for numero in numeros:
+    a= numero + 10
+    print(a)
